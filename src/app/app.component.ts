52323c591083b515186e03c2cbd32baa3ebaaf51
jasonbcox <jasonbcox@gmail.com>
2018-01-25 07:37:08 -0700

    starting on service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
+      {comment: 'first comment!', author: "Bobbi B"},
+      {comment: 'nice work!', author: "Jimmy J"},
+      {comment: 'I would also like to congratulate you!', author: "Mickey M"},
   ];
+
+  author;
+  text;
+commentToEdit;
+
+newComment={
+    author: "",
+    comment: "",
+  };
+
+  addComment(){
+    this.newComment={
+      author: this.author,
+      comment: this.text,
+    };
+
+    this.comments.push(this.newComment)
+  
+
+    
+    // console.log(this.author)
+    // console.log(this.text)
+    // console.log(this.newComment)
+    // console.log(this.comments)
+
+  }
+
+  deleteComment(i){
+    console.log(i)
+    this.comments.splice(i, 1);
+    // console.log(this.comments)
+    
+   
+  }
+
+makeCommentEditable(comment){
+  console.log(comment)
+  this.commentToEdit = comment;
+  
 }
+
+doneEditing(comment){
+  // this.comments.push(this.newComment)
+  this.commentToEdit = comment;
+}
+
+
+};
